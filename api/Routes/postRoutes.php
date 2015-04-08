39204ec5aaeab89d29774f8ddf9e9fb71165cf0e
root <parag.dakle@gslab.com>
2015-04-08 19:47:46 +0530

    updated postRoutes.php

--- api/Routes/postRoutes.php
+++ api/Routes/postRoutes.php
@@ -2,9 +2,15 @@
 
 $app->post('/register/:username/:password', function($username, $password) {
 	
+    echo "Username: " . $username . " Password: " . $password; 
 	//1. Decrypt password
 
 	//2. Add user to database
 
 	//return 200 Ok
+});
+
+$app->post('/register', function() {
+
+    print_r($_REQUEST);
 });
