007c5d850b3cc250b6e506645174b678d9cd05a7
paragdakle <parag.dakle@gslab.com>
2015-04-09 15:07:00 +0530

    bug fix for improper class declaration

--- api/Classes/mongodb.php
+++ api/Classes/mongodb.php
@@ -2,59 +2,59 @@
 
 class MongoDB
 {
-	$connection = null;
-	$db = null;
-	$collection = null;
+	private $connection = null;
+	private $db = null;
+	private $collection = null;
 	
-	function __construct($dbname, $colname) {
+	public function __construct($dbname, $colname) {
 		$connection = new MongoClient();
 		$db = $connection->selectDB($dbname);
 		$collection = $db->selectCollection($colname);
 	}
 	
-	function insertOne($document) {
+	public function insertOne($document) {
 		$collection->insert($document);
 		return true;
 	}
 	
-	function getOne($search_parameters) {
+	public function getOne($search_parameters) {
 		$record = null;
 		$record = $collection->findOne($search_parameters);
 		return $record;
 	}
 	
-	function getMany($document) {
+	public function getMany($document) {
 		$record = null;
 		$record = $collection->find($search_parameters);
 		return $record;
 	}
 	
-	function insertMany($document) {
+	public function insertMany($document) {
 		$collection->batchInsert($document, array('continueOnError' => true));
 		return true;
 	}
 	
-	function removeOne($criteria) {
+	public function removeOne($criteria) {
 		$collection->remove($criteria, array("justOne" => true));
 		return true;
 	}
 	
-	function removeMany($criteria) {
+	public function removeMany($criteria) {
 		$collection->remove($criteria, array("justOne" => false));
 		return true;
 	}
 	
-	function updateOne($criteria, $update_fields) {
+	public function updateOne($criteria, $update_fields) {
 		$collection->update($criteria, array('$set' => $update_fields), array("upsert" => true));
 		return true;
 	}
 	
-	function updateMany($criteria, $update_fields) {
+	public function updateMany($criteria, $update_fields) {
 		$collection->update($criteria, array('$set' => $update_fields), array("multiple" => true));
 		return true;
 	}
 	
-	public function __destruct() {
+	public public function __destruct() {
         $connection = null;
 		$db = null;
 		$collection = null;
