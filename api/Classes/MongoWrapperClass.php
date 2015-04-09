9358b6ee91f09c737197efc435657eff1bed0b61
paragdakle <parag.dakle@gslab.com>
2015-04-09 15:25:23 +0530

    updated mongodb core class

--- api/Classes/MongoWrapperClass.php
+++ api/Classes/MongoWrapperClass.php
@@ -6,61 +6,61 @@ class MongoWrapperClass
 	private $db = null;
 	private $collection = null;
 	
-	public function __construct($this->$dbname, $colname) {
-		$this->$connection = new MongoClient();
-		$this->$db = $this->$connection->selectDB($this->$dbname);
-		$this->$collection = $this->$db->selectCollection($colname);
+	public function __construct($dbname, $colname) {
+		$this->connection = new MongoClient();
+		$this->db = $this->connection->selectDB($dbname);
+		$this->collection = $this->db->selectCollection($colname);
 	}
 	
 	//methods to get data
 	public function getOne($search_parameters) {
 		$record = null;
-		$record = $this->$collection->findOne($search_parameters);
+		$record = $this->collection->findOne($search_parameters);
 		return $record;
 	}
 	
 	public function getMany($document) {
 		$record = null;
-		$record = $this->$collection->find($search_parameters);
+		$record = $this->collection->find($search_parameters);
 		return $record;
 	}
 	
 	//methods to insert data
 	public function insertOne($document) {
-		$this->$collection->insert($document);
+		$this->collection->insert($document);
 		return true;
 	}
 	
 	public function insertMany($document) {
-		$this->$collection->batchInsert($document, array('continueOnError' => true));
+		$this->collection->batchInsert($document, array('continueOnError' => true));
 		return true;
 	}
 
 	//methods to delete data	
 	public function deleteOne($criteria) {
-		$this->$collection->remove($criteria, array("justOne" => true));
+		$this->collection->remove($criteria, array("justOne" => true));
 		return true;
 	}
 	
 	public function deleteMany($criteria) {
-		$this->$collection->remove($criteria, array("justOne" => false));
+		$this->collection->remove($criteria, array("justOne" => false));
 		return true;
 	}
 
 	//methods to update data
 	public function updateOne($criteria, $update_fields) {
-		$this->$collection->update($criteria, array('$set' => $update_fields), array("upsert" => true));
+		$this->collection->update($criteria, array('$set' => $update_fields), array("upsert" => true));
 		return true;
 	}
 	
 	public function updateMany($criteria, $update_fields) {
-		$this->$collection->update($criteria, array('$set' => $update_fields), array("multiple" => true));
+		$this->collection->update($criteria, array('$set' => $update_fields), array("multiple" => true));
 		return true;
 	}
 	
 	public function __destruct() {
-        $this->$connection = null;
-        $this->$db = null;
-        $this->$collection = null;
+        $this->connection = null;
+        $this->db = null;
+        $this->collection = null;
     }
 }
