cdcec0999e77f19f3ea6a5f06cf5d2a5abb79db1
paragdakle <parag.dakle@gslab.com>
2015-04-09 11:48:43 +0530

    Addition of encryption library

--- api/Routes/postRoutes.php
+++ api/Routes/postRoutes.php
@@ -2,7 +2,14 @@
 
 $app->post('/register/:username/:password', function($username, $password) {
 	
-    echo "Username: " . $username . " Password: " . $password; 
+	include('Crypt/AES.php');
+	$cipher = new Crypt_AES();
+	
+	$cipher->setKey($username);
+	
+	echo $cipher->decrypt($password);
+	
+    //echo "Username: " . $username . " Password: " . $password; 
 	//1. Decrypt password
 
 	//2. Add user to database
