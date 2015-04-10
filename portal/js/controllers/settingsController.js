f4e0c09523991640a188e1cf6a2c63aed0a724e4
paragdakle <parag.dakle@gslab.com>
2015-04-10 19:07:34 +0530

    trying partial rendering

--- portal/js/controllers/settingsController.js
+++ portal/js/controllers/settingsController.js
@@ -4,7 +4,12 @@ var app = angular.module('settingsApp', []);
 app.controller('settingsController', function($scope, $http) {
     $scope.admin = true;
 	$scope.displayName = "parag.dakle@gmail.com";
-
+	
+	$http.get("/render/getUsers")
+		.success(function(response) {
+			$scope.body_container_content = response;
+		});
+		
 	$http.get("/settings/getUsers")
 		.success(function(response) {
 			if(response.status == 400) {
