219a6c16727330c0ba6baf3beccbf621150118f4
paragdakle <parag.dakle@gslab.com>
2015-04-10 18:02:41 +0530

    removal of getting html by rest

--- portal/js/controllers/settingsController.js
+++ portal/js/controllers/settingsController.js
@@ -4,12 +4,7 @@ var app = angular.module('settingsApp', []);
 app.controller('settingsController', function($scope, $http) {
     $scope.admin = true;
 	$scope.displayName = "parag.dakle@gmail.com";
-	
-	$http.get("/render/getUsers")
-		.success(function(response) {
-			$("#body-container").get(0).innerHTML = response;
-		});
-		
+
 	$http.get("/settings/getUsers")
 		.success(function(response) {
 			if(response.status == 400) {
