03afcbc7fee9fa1b3b4175a18420983964ce7e75
paragdakle <parag.dakle@gslab.com>
2015-04-10 19:35:03 +0530

    removal of template rendering

--- portal/js/controllers/settingsController.js
+++ portal/js/controllers/settingsController.js
@@ -4,11 +4,6 @@ var app = angular.module('settingsApp', []);
 app.controller('settingsController', function($scope, $http) {
     $scope.admin = true;
 	$scope.displayName = "parag.dakle@gmail.com";
-	
-	$http.get("/render/getUsers")
-		.success(function(response) {
-			$scope.body_container_content = response;
-		});
 		
 	$http.get("/settings/getUsers")
 		.success(function(response) {
